ea4b1042613b54dd1b9037b98e1faea37ad8a439
lovelylucia <lovelylucia00@gmail.com>
2022-03-02 23:52:25 +0900

    220302_learn state grammar

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let posts = '강남 고기 맛집'
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+
+  let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className="black-nav">
-        개발 blog
+        <div style={ { color:'blue', fontSize:'30px'} }>개발 blog</div>
       </div>
-      <h4>{ posts }</h4>
+      <div className="list">
+        <h3> { 글제목 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
