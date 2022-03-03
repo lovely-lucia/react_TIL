4039ba1037293a98b0818a0339140d530b5cd604
lovelylucia <lovelylucia00@gmail.com>
2022-03-03 23:52:28 +0900

    220303_learn state grammar second

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,15 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState('남자 코트 추천');
-
+  let [글제목, 글제목변경] = useState(
+    ['내가 나이기 싫은 날',
+    '나이는 그만 먹고 싶다',
+    '프로필 사진을 찍었습니다'
+  ]);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   return (
@@ -12,21 +17,23 @@ function App() {
       <div className="black-nav">
         <div style={ { color:'blue', fontSize:'30px'} }>개발 blog</div>
       </div>
+      <button onClick={ () => {}}> 제목변경 </button>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목 [0]} <span onClick={ () => { 따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목 [1]} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목 [2]} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      
     </div>
   );
 }
