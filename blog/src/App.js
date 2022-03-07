3fe77a89cc9a5ddfb36dea47cd92f5b919dd9196
lovelylucia <lovelylucia00@gmail.com>
2022-03-07 23:49:17 +0900

    220307_learn props

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(
-    ['내가 나이기 싫은 날',
-    '나이는 그만 먹고 싶다',
-    '프로필 사진을 찍었습니다'
+  let [글제목, 글제목변경] = useState(['내가 나이기 싫은 날','나이는 그만 먹고 싶다','프로필 사진을 찍었습니다'
   ]);
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
@@ -27,7 +24,7 @@ function App() {
         <div style={ { color:'blue', fontSize:'30px'} }>개발 blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목 [0]} <span onClick={ () => { 따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
+        <h3> { 글제목 [0]} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
@@ -45,7 +42,7 @@ function App() {
       
       {
         modal === true 
-        ? <Modal /> 
+        ? <Modal 글제목={글제목} ></Modal> 
         : null
       } 
        
@@ -53,7 +50,7 @@ function App() {
         글제목.map(function(글){
           return (
             <div className="list">
-              <h3> { 글 } </h3>
+              <h3> { 글 } <span onClick={ () => { 따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
               <p>2월 17일 발행</p>
               <hr/>
             </div>
@@ -64,10 +61,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-        <h2>제목</h2>
+        <h2>제목 { props.글제목[0] }</h2>
         <p>날짜</p>
         <p>상세내용</p>
     </div>
