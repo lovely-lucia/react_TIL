8ed3c21b88ba72f2918d96057f3b1172141cfd7a
lovely-lucia <lovelylucia00@gmail.com>
2022-03-22 17:29:47 +0900

    220322_add_Detail.scss

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,20 +1,40 @@
 import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss'
+
+let Box = styled.div`
+      padding:20px;
+      background:#f1f1f1;
+    `;
+
+let 제목 = styled.h4`
+  font-size:25px;
+`
 
 function Detail(props){
-
+    
+    let { id } = useParams();
     let history = useHistory();
+
+    let 찾은상품 = props.shoes.find(function(상품){
+      return 상품.id == id  
+    });
 
     return(
     <div className="container">
+        <Box>
+          <제목 className='red'>상세페이지</제목>
+        </Box>
         <div className="row">
           <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg' } width='100%' alt='신발이미지'/>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%' alt='신발이미지'/>
           </div>
           <div className="col-md-6 mt-4">
-            <h4 className="pt-5">{props.shoes[0].title}</h4>
-            <p>{props.shoes[0].content}</p>
-            <p>{props.shoes[0].price}원</p>
+            <h4 className="pt-5">{찾은상품.title}</h4>
+            <p>{찾은상품.content}</p>
+            <p>{찾은상품.price}원</p>
             <button className="btn btn-danger">주문하기</button> 
             <button className="btn btn-danger" onClick={()=>{
                 history.push('/');
